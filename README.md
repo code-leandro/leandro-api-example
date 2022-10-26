143a8e3ec81bf80f917c2288630c35c4b9a9e40e
Leandro Alves de Souza <dbc.leandro@unimedpoa.com.br>
2022-10-25 23:39:14 -0300

    Added Swagger

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
+
 ## Executar o projeto
 
 ```
 $ mvn spring-boot:run
 ```
 
-## Executar apenas os testes unitários
+## Swagger
+Após a execução local, é possível acessar o swagger no link:
+[Link Swagger on Localhost](https://localhost:8080/swagger-ui/index.html)
+
+## Para executar apenas os testes unitários
 ```
 $ mvn test
 ```
--- src/main/java/com/leandrosouza/leandroagrotis/config/ConfigBeans.java
+++ src/main/java/com/leandrosouza/leandroagrotis/config/ConfigBeans.java
@@ -4,11 +4,17 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Locale;
 
 @Configuration
 public class ConfigBeans {
+
+    public static final String BASE_PACKAGE_SWAGGER = "com.leandrosouza.leandroagrotis.api";
 
     @Bean
     public MessageSource messageSource() {
@@ -21,5 +27,14 @@ public class ConfigBeans {
     @Bean
     public Locale locale() {
         return new Locale("pt", "BR");
+    }
+
+    @Bean
+    public Docket myApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE_SWAGGER))
+                .paths(PathSelectors.any())
+                .build();
     }
 }
